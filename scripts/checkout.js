3e939187786517a22a9e1620dd05f6b1a4422744
AbdullahAshraff <125521810+AbdullahAshraff@users.noreply.github.com>
2024-08-03 15:09:03 +0300

    edit cart-class.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import renderCheckoutHeader from './checkout/checkout-header.js';
 import renderCartSummary from './checkout/cart-summary.js';
 import renderPaymentSummary from './checkout/payment-summary.js';
-import '../data/cart-class.js';
+// import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderCartSummary();
