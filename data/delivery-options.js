db7c10fc7b359d77c716a5bf4a5ac8b3abb14511
AbdullahAshraff <125521810+AbdullahAshraff@users.noreply.github.com>
2024-08-05 08:56:00 +0300

    created class for deliverOption

--- data/delivery-options.js
+++ data/delivery-options.js
@@ -1,20 +1,38 @@
+class DeliveryOption {
+    id;
+    deliveryDays;
+    priceCents;
+
+    constructor(optionDetails) {
+        this.id = optionDetails.id;
+        this.deliveryDays = optionDetails.deliveryDays;
+        this.priceCents = optionDetails.priceCents;
+    }
+}
+
 export const deliveryOptions = [
     {
-        id: "1",
+        id: '1',
         deliveryDays: '7',
-        priceCents: 0
-    },{
-        id: "2",
-        deliveryDays: '3',
-        priceCents: 499
-    },{
-        id: "3",
-        deliveryDays: '1',
-        priceCents: 999
+        priceCents: 0,
     },
-];
+    {
+        id: '2',
+        deliveryDays: '3',
+        priceCents: 499,
+    },
+    {
+        id: '3',
+        deliveryDays: '1',
+        priceCents: 999,
+    },
+].map(optionDetails => {
+    return new DeliveryOption(optionDetails);
+});
 
-export function getDeliveryOption( productId){
-
-    return deliveryOptions.find(option => productId === option.id) || deliveryOptions[0];
+export function getDeliveryOption(productId) {
+    return (
+        deliveryOptions.find(option => productId === option.id) ||
+        deliveryOptions[0]
+    );
 }
